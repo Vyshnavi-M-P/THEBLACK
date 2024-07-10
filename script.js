bf750739fb58ad817bd2f19b2c64fbf3f62663fd
Vyshnavi-M-P <minugusnehith@gmail.com>
2024-07-10 21:32:49 +0530

    Add files via upload
    TASK1-Basic HTML and CSS Website

---
+++ script.js
@@ -0,0 +1,7 @@
+const learnMoreButton = document.getElementById("learn-more");
+
+learnMoreButton.addEventListener("click", function() {
+  window.location.href = "learnmore.html"; // Replace with your learnmore page name
+});
+
+
